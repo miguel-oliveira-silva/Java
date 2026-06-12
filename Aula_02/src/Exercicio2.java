b49e23204ecba11eb1804426dd126c1ad2f8559a
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2026-06-12 14:18:05 -0300

    Dia 2

--- Aula_02/src/Exercicio2.java
+++ Aula_02/src/Exercicio2.java
@@ -3,10 +3,11 @@ public class Exercicio2 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		float media = 0	;
-		for(int i = 0; i < 5; i++) {
+		for(int i = 1; i < 5; i++) {
 			System.out.printf("\nNota %d: ", i);
-			media =+ scan.nextFloat();
+			media = media + scan.nextFloat();
 		}
+		media = media/4;
 		System.out.printf("\nMédia final: %.2f", media);
 	}
 }
