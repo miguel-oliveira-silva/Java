b1d879d3ebbe19cff0929e1cb92b78d62f7fbd99
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2026-06-15 15:22:31 -0300

    Dia 3

---
+++ Aula_03/src/Exercicio_5.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Exercicio_5 {
+	public static void main(String[] args) {
+		final String[] PRODUTOS = {"vazio","Cachorro Quente", "X-Salada","X-Bacon","Bauru","Refrigerante","Suco de laranja"};
+		Scanner scanner = new Scanner(System.in);
+		int preco = 0, produto, quantidade;
+		System.out.println("Código do Produto: ");
+		produto = scanner.nextInt();
+		if(produto < 1 || produto > 7 ) {
+			System.out.println("Código invalido");
+			scanner.close();
+			return;
+		}
+		System.out.println("Quantidade: ");
+		quantidade = scanner.nextInt();
+		switch(produto) {
+		
+			case 1:
+				preco = 10;	
+				break;
+			case 2:
+				preco = 15;	
+				break;
+			case 3:
+				preco = 18;	
+				break;
+			case 4:
+				preco = 12;	
+				break;
+			case 5:
+				preco = 8;	
+				break;
+			case 6:
+				preco = 13;	
+				break;
+			default:
+				System.out.println("Inválido");
+				scanner.close();
+				return;
+		}
+		System.out.printf("Produto: %s", PRODUTOS[produto]);
+		System.out.printf("Valor total: %d", preco*quantidade);
+		scanner.close();
+	}
+}
